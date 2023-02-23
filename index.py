26b81a9d7e485d90031d1c8eb5c09f852bba6c48
ShibaTheBuba <126165092+ShibaTheBuba@users.noreply.github.com>
2023-02-23 12:30:14 +0000

    eeeee

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+import random
+import pandas as pd
+
+num = 1
+num2 = eee
+
+yeah = random.randint(num, num2)
+if num = isnull():
+    print("Pik nume pks")
+elif num2  = pd.isnull():
+    print("hi")
+
+print(yeah)
